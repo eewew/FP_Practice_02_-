075fef4b32a0484edebb10a7a1b6635dc3cd75e8
vovan <vovan@DESKTOP-NMJPF1J>
2023-01-23 15:36:30 +0200

    Add project files.

---
+++ FP_Practice_02_Вступ.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "\n\tTask 1\n\n";
+    cout << "Name\t\t: \"The War And Piece\"\n";
+    cout << "Author\t\t: L.N. Tolstoj\n";
+    cout << "Izdatelstvo\t: Piter\n";
+    cout << "Pages\t\t: 500\n";
+    cout << "\n\tTask 2\n\n";
+    cout << "\"Kraj lukomor’a dub zelenyj, \a\n";
+    cout << "I zolotyj lancyuh na nim:\a\n";
+    cout << "Shodnya, shonochi kit uchenyj\a\n"; 
+    cout << "Na lancyuhu kruzhl\'aje tim\"\a\n"; 
+    cout << "\n\tTask 3\n\n";
+    cout <<(char)201<<(char)205<<(char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 <<(char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205<<(char)205 << (char)205 << (char)187;
+    cout << "\n";
+    cout << (char)186  << "\t\t\t\t\t\t"  << (char)186 << "\n"  << (char)186;
+    cout << "\t\t  Pory roku" << "\t\t\t" <<(char)186<<"\n";
+    cout << (char)198 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)216;
+    cout << "\n";
+    cout << (char)186 << "\t\t\t\t\t\t" << (char)186 << "\n" << (char)186;
+    cout << "  Zyma\t" << (char)186;
+    cout << "  Vesna\t" << (char)186;
+    cout << "  Lito\t" << (char)186;
+    cout << "  Osin\t\n" << (char)186;
+    cout  << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 <<(char)186;
+}
+
